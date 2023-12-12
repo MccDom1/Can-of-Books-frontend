b52a5fc8188e5979fd332b3e1f7ea8f2c1f8630b
DOMINIQUE MCCLANEY <MCCLANEYDOMINIQUE@GMAIL.COM>
2023-12-11 19:21:26 -0500

    adding updated to book1 branch

--- src/About.jsx
+++ src/About.jsx
@@ -4,7 +4,19 @@ class Profile extends Component {
 
   render() {
     /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
+    return(
+      <>
+        <h1>AboutUs</h1>
+        <h2>Dominique McClaney</h2>
+        <p>Hi, I'm Dominique McClaney</p>
+        <a href="https://github.com/MccDom1">My Github</a>
+
+        <h2>Immanuel Shin</h2>
+        <p>Hi, I'm Immanuel Shin</p>
+        <a href="https://github.com/ImmanuelShin">My Github</a>
+      </>
+      
+    ) 
   }
 }
 
