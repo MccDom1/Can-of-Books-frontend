1944d3acb543b4d4a8fe3332667cd30a4a659e65
Immanuel Shin <Immanuel.shin18@gmail.com>
2023-12-13 00:06:13 +0000

    Corrected URI

--- src/BestBooks.jsx
+++ src/BestBooks.jsx
@@ -2,15 +2,15 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import Carousel from 'react-bootstrap/Carousel';
 import { useState } from 'react';
-import  Button  from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button';
 import BookFormModal from './BookFormModal';
 /* TODO: Create a component called `BestBooks` that renders a Carousel of all the books in your database */
 function BestBooks() {
 
   const [books, setBooks] = useState([]);
   const [newBook, setNewBook] = useState({});
-const [show, setShow] = useState(false);
-  
+  const [show, setShow] = useState(false);
+
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
   async function getBooks() {
     try {
@@ -31,11 +31,11 @@ const [show, setShow] = useState(false);
     <>
       <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
       <Button variant="primary" onClick={() => setShow(true)}>add book</Button>
-      {show && <BookFormModal 
-      show={show} 
-      setShow={setShow}
-      setBooks={setBooks}
-      />} 
+      {show && <BookFormModal
+        show={show}
+        setShow={setShow}
+        setBooks={setBooks}
+      />}
       {books.length ? (
         <Carousel>
           {books.map((book) => (
